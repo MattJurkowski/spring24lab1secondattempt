16e9f9bb5602b986e27ab8553e582dae58c1e410
MattJurkowski <jurek198@gmail.com>
2024-04-07 11:13:51 +0000

    lab1 code working
    Signed-off-by: MattJurkowski <jurek198@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,19 +1,25 @@
 #include "mbed.h"
+#include <cstdio>
 
-
+// line below sets our button
 InterruptIn buttn(PC_13);
+// volatile was added on front of integer as program was ignoring myflag
+volatile int myflag = 0;
 
 void buttn_fall(){
-    printf("button pressed!!\r\n");
+    myflag = 1;
 }
 
 // main() runs in its own thread in the OS
 int main()
 {
     printf("This is mbed os v%d \r\n",MBED_VERSION);
-
-    buttn.fall(&buttn_fall);
+    buttn.fall(&buttn_fall); //in here code says to print "button pressed!!" only when button is pressed
     while (true) {
+        if(myflag == 1){
+            printf("button pressed!!\r\n");
+            myflag = 0;
+        }
 
     }
 }
