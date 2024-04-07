e7a5bdf4aea43f5bbb7e48796556a47ca5f304f7
MattJurkowski <jurek198@gmail.com>
2024-04-07 11:00:46 +0000

    lab1part1programwitherror
    Signed-off-by: MattJurkowski <jurek198@gmail.com>

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include "mbed.h"
+
+
+InterruptIn buttn(PC_13);
+
+void buttn_fall(){
+    printf("button pressed!!\r\n");
+}
+
+// main() runs in its own thread in the OS
+int main()
+{
+    printf("This is mbed os v%d \r\n",MBED_VERSION);
+
+    buttn.fall(&buttn_fall);
+    while (true) {
+
+    }
+}
+
